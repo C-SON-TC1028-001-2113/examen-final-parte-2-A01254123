daa9356d05f3967a446ddaef29534adda4dac032
A01254123 <A01254123@itesm.mx>
2021-10-15 18:00:02 +0000

    Cesar

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,8 +1,26 @@
-
+def matrizppal():
+    lista=[]
+    fila= int(input())
+    columna=int(input())
+    if fila>0 and columna>0: 
+        for i in range(fila):
+            lista.append([])
+            for j in range(columna):
+                valores=int(input())
+                lista[i].append(valores)
+    else: 
+        print('Error')
+    return lista
 
 def main():
-
-
-
+    matriz = matrizppal()
+    listaColumna = []
+    if len(matriz) > 0:
+        for i in range(len(matriz[0])):
+            count = 0
+            for j in range(len(matriz)):
+                count += matriz[j][i] 
+            listaColumna.append(count)
+        print(listaColumna)
 if __name__=='__main__':
     main()
